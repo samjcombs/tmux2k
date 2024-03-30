856d68f4cf3ac6badd703958859b5e43fc352073
samjcombs <104232548+samjcombs@users.noreply.github.com>
2024-03-30 18:16:08 -0400

    Update weather.sh

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -72,9 +72,9 @@ display_weather() {
 main() {
     # process should be cancelled when session is killed
     if ping -q -c 1 -W 1 ipinfo.io &>/dev/null; then
-        echo "$(display_weather) $(display_location)"
+        echo "$(display_weather)"
     else
-        echo "Location Unavailable"
+        echo "󰟦"
     fi
 }
 
